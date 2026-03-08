50e388e7f0d4535afe55d2ec648f09b419e65941
Arif Reja <arifreja643@gmail.com>
2026-03-08 22:10:10 +0530

    Update Holdings.js

--- dashboard/src/Holdings.js
+++ dashboard/src/Holdings.js
@@ -7,7 +7,7 @@ const Holdings = () => {
   const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
-    axios.get("http://localhost:3001/allHoldings")
+    axios.get("http://localhost:3002/allHoldings")
       .then((res) => {
         setAllHoldings(res.data);
         setLoading(false);
